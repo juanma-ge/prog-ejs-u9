9045f962a5b74ce335a93507ba02606bde341f7a
juanma-ge <jgomest545@g.educaand.es>
2025-05-13 19:25:17 +0200

    Documentación y uso de try and catch

--- src/main/kotlin/app/ConsUsuario.kt
+++ src/main/kotlin/app/ConsUsuario.kt
@@ -1,13 +1,21 @@
 package org.example.app
 
 import org.example.data.DataBase.getConnection
+import java.sql.SQLException
 
 class ConsUsuario {
 
+    /**
+     * Muestra todos los usuarios los cuales hayan comprado el producto 'Abanico'.
+     *
+     * Se ejecuta una consulta en SQL para encontrar los usuarios.
+     * En caso de error o excepción se capturará.
+     */
     fun mostrarUsuarios(){
-        val conn = getConnection()
+        try {
+            val conn = getConnection()
 
-        val stmt = conn?.prepareStatement("""
+            val stmt = conn?.prepareStatement("""
         SELECT DISTINCT u.nombre            
         FROM Usuario u
         JOIN Pedido pe ON u.id = pe.idUsuario 
@@ -15,12 +23,15 @@ class ConsUsuario {
         JOIN Producto pr ON lp.idProducto = pr.id 
         WHERE pr.nombre = ?               
         """)
-        stmt?.setString(1, "Abanico")
+            stmt?.setString(1, "Abanico")
 
-        val rs = stmt?.executeQuery()
+            val rs = stmt?.executeQuery()
 
-        if (rs != null) {
-            while (rs.next()){println("Usuarios que han comprado el producto 'Abanico': ${rs.getString("nombre")}")}
+            if (rs != null) {
+                while (rs.next()){println("Usuarios que han comprado el producto 'Abanico': ${rs.getString("nombre")}")}
+            }
+        }catch (e: SQLException){
+            println("Error al ejecutar la consulta: ${e.message}")
         }
     }
 
